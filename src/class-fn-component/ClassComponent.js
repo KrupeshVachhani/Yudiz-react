23d019888a04886d8a5b573279bda085828fc6e7
KrupeshVachhani <vachhani.krupesh@gmail.com>
2024-11-07 14:35:48 +0530

    first commit

---
+++ src/class-fn-component/ClassComponent.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+
+class ClassComponent extends Component {
+
+  state = {
+    count: 0,
+    color: 'blue',
+    isVisible: true
+  };
+
+  increment = () => this.setState({ count: this.state.count + 1 });
+
+  changeColor = () => this.setState({ color: this.state.color === 'blue' ? 'pink' : 'blue' });
+
+  toggleVisibility = () => this.setState({ isVisible: !this.state.isVisible });
+
+  render() {
+
+    const { count, color, isVisible } = this.state;
+
+    return (
+      <div>
+        <h2>Class Component</h2>
+        <p>Count: {count}</p>
+        <button onClick={this.increment}>Increment</button>
+
+        <p style={{ color: color }}>Color</p>
+
+        <button onClick={this.changeColor}>Change Color</button>
+
+        <button onClick={this.toggleVisibility}>Toggle Visibility</button>
+        {isVisible && <p>Visible Content</p>}
+      </div>
+    );
+  }
+}
+
+
+export class ClassComponentP extends Component {
+  render() {
+
+    const { name, description, imageUrl, onClickHandler } = this.props;
+
+    return (
+      <div className="card" onClick={onClickHandler}>
+        <img src={imageUrl} alt={name} className="card-image" />
+        <h3 className="card-title">{name}</h3>
+        <p className="card-description">{description}</p>
+      </div>
+    );
+  }
+}
+
+
+
+export default ClassComponent;
---
+++ src/class-fn-component/FunctionalComponent.js
@@ -0,0 +1,55 @@
+// Props and Methods Organization
+// Code Reuse and Logic Sharing
+// Lifecycle Method Simplification
+// State Management Simplification
+
+
+import React, { useState } from 'react';
+
+const FunctionalComponent = () => {
+  const [count, setCount] = useState(0);
+  const [color, setColor] = useState('pink');
+  const [isVisible, setIsVisible] = useState(true)
+
+  const increment = () => setCount(count + 1);
+
+  const changeColor = () => color === 'pink' ? setColor('blue') : setColor('pink');
+
+  const toggleVisibility = () => setIsVisible(!isVisible);
+
+  return (
+    <div>
+      <h2>Functional Component</h2>
+      <p>Count: {count}</p>
+      <button onClick={increment}>Increment</button>
+
+
+      <p style={{ color: color }}>Color</p>
+
+      <button onClick={changeColor}>Change Color</button>
+
+      <button onClick={toggleVisibility}>Toggle Visibility</button>
+      {isVisible && <p>Visible Content</p>}
+    </div >
+  );
+};
+
+const ABcd = (props) => {
+  return (
+    <>Hello{props.name}</>
+  )
+}
+
+export const FunctionComponentP = ({ name, description, imageUrl, onClickHandler }) => {
+  return (
+    <>
+    <ABcd name={name} />
+    <div className="card" onClick={onClickHandler}>
+      <img src={imageUrl} alt={name} className="card-image" />
+      <h3 className="card-title">{name}</h3>
+      <p className="card-description">{description}</p>
+    </div></>
+  );
+};
+
+export default FunctionalComponent;
